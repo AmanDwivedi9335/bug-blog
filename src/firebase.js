5e324ce97b9b7430d8e8d822ae8cf899bf79be36
Aman <amandwdi@gmail.com>
2023-11-01 19:37:49 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWOnPdPuS-fMgLMYeBK9S1EALnYGE_98o",
+  authDomain: "bug-blog-3512f.firebaseapp.com",
+  projectId: "bug-blog-3512f",
+  storageBucket: "bug-blog-3512f.appspot.com",
+  messagingSenderId: "283841566484",
+  appId: "1:283841566484:web:692f94cd532852f6d35401"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
