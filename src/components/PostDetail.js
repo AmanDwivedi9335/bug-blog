ebf55abf98c2e05811a138ff3fabff46e8138360
Aman <amandwdi@gmail.com>
2023-11-20 08:19:10 +0530

    Create post and view post is updated

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,9 +1,37 @@
-function PostDetail() {
-    return (
-      <div className="">
-        Postdetail
-      </div>
-    );
-  }
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from "../firebase";
 
-  export default PostDetail;
+function PostDetail() {
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const postCollection = collection(db, 'posts');
+        const postSnapshot = await getDocs(postCollection);
+        postSnapshot.forEach((doc) => {
+          if (doc.id === postId) {
+            console.log('snapshot', doc.data());
+            setPost(doc.data());
+          }
+        });
+      } catch (error) {
+        console.error("Error fetching post:", error);
+      }
+    };
+
+    fetchPost();
+  }, [postId]);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
